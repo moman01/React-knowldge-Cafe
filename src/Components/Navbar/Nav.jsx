f66d77018059e078e8118c5564eb6b343f78550c
moman01 <learnermoman@gmail.com>
2025-01-24 23:40:35 +0600

    project start

---
+++ src/Components/Navbar/Nav.jsx
@@ -0,0 +1,14 @@
+
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <h3 className="text-2xl font-bold">Knowldge cafe</h3>
+                <img src="../../assets/images/boy1.png" alt=""  />
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
